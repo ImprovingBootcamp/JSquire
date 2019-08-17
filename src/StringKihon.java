ccea8bc92e78bf101b103fb2780b8eb198d678a8
Tim Rayburn <tim@timrayburn.net>
2019-08-17 16:58:28 -0500

    String Kihon

---
+++ src/StringKihon.java
@@ -0,0 +1,76 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class StringKihon extends StringKihonBase {
+
+    @Override
+    protected String Convert_To_Uppercase(String data) {
+        return null;
+    }
+
+    @Override
+    protected String Convert_To_Lowercase(String data) {
+        return null;
+    }
+
+    @Override
+    protected String Combine_Parts_Of_A_Name(String firstName, String middleName, String lastName) {
+        return null;
+    }
+
+    @Override
+    protected String Combine_Two_Strings(String a, String b) {
+        return null;
+    }
+
+    @Override
+    protected int Determine_The_Length_Of_A_String(String data) {
+        return 0;
+    }
+
+    @Override
+    protected String Remove_All_Leading_Whitespace(String data) {
+        return null;
+    }
+
+    @Override
+    protected String Remove_All_Trailing_Whitespace(String data) {
+        return null;
+    }
+
+    @Override
+    protected String[] Split_A_String_Into_An_Array(String input, char divider) {
+        return new String[0];
+    }
+
+    @Override
+    protected String Join_An_Array_Into_A_String(String[] input, String divider) {
+        return null;
+    }
+
+    @Override
+    protected boolean Return_True_If_a_Contains_b(String a, String b) {
+        return false;
+    }
+
+    @Override
+    protected int Determine_The_Position_Of_a_In_b(String a, String b) {
+        return 0;
+    }
+
+    @Override
+    protected boolean Return_True_If_a_Starts_With_b(String a, String b) {
+        return false;
+    }
+
+    @Override
+    protected boolean Return_True_Is_a_Ends_With_b(String a, String b) {
+        return false;
+    }
+
+    @Override
+    protected String Return_The_Fourth_Through_Seventh_Characters_Of_Input(String input) {
+        return null;
+    }
+}

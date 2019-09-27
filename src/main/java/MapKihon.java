201ef80a3c745a1c4923bc3226cc264274caae1c
Austin Briggs <Austin.W.Briggs@gmail.com>
2019-09-27 13:44:39 -0500

    Adding map kihon

---
+++ src/main/java/MapKihon.java
@@ -0,0 +1,32 @@
+import java.util.Map;
+
+import framework.MapKihonBase;
+
+public class MapKihon extends MapKihonBase {
+
+    @Override
+    protected Map<String, String> createMapOfStringToString() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void addKeyAndValueToMap(Map<String, String> inputMap, String key, String value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected String getTheValueAtThisKey(Map<String, String> inputMap, String key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected boolean doesMapContainKey(Map<String, String> inputMap, String input) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeKeyFromMap(Map<String, String> inputMap, String key) {
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ src/main/java/framework/MapKihonBase.java
@@ -0,0 +1,82 @@
+package framework;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+public abstract class MapKihonBase {
+    protected abstract Map<String, String> createMapOfStringToString();
+    protected abstract void addKeyAndValueToMap(Map<String, String> inputMap, String key, String value);
+    protected abstract String getTheValueAtThisKey(Map<String, String> inputMap, String key);
+    protected abstract boolean doesMapContainKey(Map<String, String> inputMap, String input);
+    protected abstract void removeKeyFromMap(Map<String, String> inputMap, String key);
+
+    @Test
+    public void createMapOfStringToStringTest() {
+        // Arrange
+
+        // Act
+        Map<String, String> newMap = createMapOfStringToString();
+
+        // Assert
+        assertNotNull(newMap);
+    }
+
+    @Test
+    public void putKeyAndValueToMapTest() {
+        // Arrange
+        Map<String, String> map = new HashMap<>();
+
+        // Act
+        addKeyAndValueToMap(map, "austin", "hello");
+
+        // Assert
+        assertEquals("hello", map.getOrDefault("austin", ""));
+
+    }
+
+
+    @Test
+    public void getTheValueAtThisKeyTest() {
+        // Arrange
+        Map<String, String> map = Map.of("improving", "Improving Bootcamp");
+
+        // Act
+        String value = getTheValueAtThisKey(map, "improving");
+
+
+        // Assert
+        assertEquals("Improving Bootcamp", value);
+    }
+
+    @Test
+    public void doesMapContainKeyTest() {
+        // Arrange
+        Map<String, String> map = Map.of("improving", "Improving Bootcamp");
+
+        // Act
+        boolean result = doesMapContainKey(map, "hello");
+
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    public void removeKeyFromMapTest() {
+        // Arrange
+        Map<String, String> map = Map.of("improving", "Improving Bootcamp");
+
+        // Act
+        removeKeyFromMap(map, "improving");
+
+        // Assert
+        assertTrue(map.isEmpty());
+    }
+
+}

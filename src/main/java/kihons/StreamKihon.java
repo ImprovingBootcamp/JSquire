61a13ac6f05fe587294325e45d03da4d97356ac3
akithas <alex.kithas@improving.com>
2019-10-04 14:48:41 -0500

    remove answers

--- src/main/java/kihons/StreamKihon.java
+++ src/main/java/kihons/StreamKihon.java
@@ -4,10 +4,8 @@ import framework.Item;
 import framework.bases.StreamsKihonBase;
 import framework.exceptions.NotImplementedYetException;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class StreamKihon extends StreamsKihonBase {
 
@@ -23,21 +21,21 @@ public class StreamKihon extends StreamsKihonBase {
 
     @Override
     protected List<Item> sortOnCostFromLestToMostExpensiveAllItemsThatCostLessThanOrEqualToMyMonies(List<Item> items, int monies) {
-        return items.stream().sorted(Comparator.comparing(Item::getCost)).filter(i -> i.getCost() <= monies).collect(Collectors.toList());
+        throw new NotImplementedYetException();
     }
 
     @Override
     protected Item getTheItemWithTheMostCost(List<Item> items) {
-        return items.stream().sorted(Comparator.comparing(Item::getCost).reversed()).findFirst().get();
+        throw new NotImplementedYetException();
     }
 
     @Override
     protected int getTheAverageCostRoundedDown(List<Item> items) {
-        return items.stream().map(i -> i.getCost()).reduce(0, (ans, cur) -> ans += cur)/items.size();
+        throw new NotImplementedYetException();
     }
 
     @Override
     protected void addAllItemsToMyMapWIthKetNameAndValueOfCost(List<Item> items, Map<String, Integer> myMap) {
-        items.stream().forEach(i -> myMap.put(i.getName(), i.getCost()));
+        throw new NotImplementedYetException();
     }
 }

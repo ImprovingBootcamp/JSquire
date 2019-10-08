5426b8c9d44b176978eb93c475a5f9e3c8ee68dd
Zach Cannon <Zach.Cannon@gmail.com>
2019-10-08 08:55:27 -0500

    Removing answer from sql ddl kihone

--- src/main/java/kihons/SqlDdlKihon.java
+++ src/main/java/kihons/SqlDdlKihon.java
@@ -1,17 +1,11 @@
 package kihons;
 
 import framework.bases.SqlDdlKihonBase;
+import framework.exceptions.NotImplementedYetException;
 
 public class SqlDdlKihon extends SqlDdlKihonBase {
     @Override
     public String createTablePersonWithOneColumn() {
-        var sb = new StringBuilder();
-
-        sb.append("CREATE TABLE Person");
-        sb.append("(");
-        sb.append("PersonId int not null PRIMARY KEY ");
-        sb.append(")");
-
-        return sb.toString();
+        throw new NotImplementedYetException();
     }
 }
